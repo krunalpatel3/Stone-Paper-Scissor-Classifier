a1bc1918ee8f5b1b305d501d669693adaf7da28d
krunalpatel3 <patelkrunal821@gmail.com>
2021-07-19 22:40:00 +0530

    -initial commit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def tell(classes):
         # print('Its a Paper')
 
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
